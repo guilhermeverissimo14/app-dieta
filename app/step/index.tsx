f8ac53612f4d7644652e3938c1dc5f5fc1a1c2e6
guilhermeverissimo14 <guilhermeverissimo14@gmail.com>
2024-09-18 10:27:24 -0300

    feat:add step 1

--- app/step/index.tsx
+++ app/step/index.tsx
@@ -1,8 +1,30 @@
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
 import { Header } from '@/components/header';
+import { Input } from '@/components/input';
 import { colors } from '@/constants/Colors';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+const schema = z.object({
+    name: z.string().min(1, { message: 'O nome é obrigatório' }),
+    weight: z.string().min(1, { message: 'O peso é obrigatório' }),
+    age: z.string().min(1, { message: 'A idade é obrigatória' }),
+    height: z.string().min(1, { message: 'A altura é obrigatória' }),
+
+})
+
+type FormData = z.infer<typeof schema>;
 
 export default function Step() {
+
+    const { control, handleSubmit, formState: { errors, isValid } } = useForm<FormData>({
+        resolver: zodResolver(schema)
+    })
+
+
     return (
         <View style={styles.container}>
 
@@ -12,6 +34,46 @@ export default function Step() {
             />
 
             <ScrollView style={styles.content}>
+
+                <Text style={styles.label}>Nome:</Text>
+                <Input
+                    name='name'
+                    control={control}
+                    placeholder='Digite seu nome...'
+                    error={errors.name?.message}
+                    keyboardType='default'
+                />
+
+                <Text style={styles.label}>Seu peso atual:</Text>
+                <Input
+                    name='weight'
+                    control={control}
+                    placeholder='Ex: 75'
+                    error={errors.weight?.message}
+                    keyboardType='numeric'
+                />
+
+                <Text style={styles.label}>Seu altura atual:</Text>
+                <Input
+                    name='height'
+                    control={control}
+                    placeholder='Ex: 1.90'
+                    error={errors.height?.message}
+                    keyboardType='numeric'
+                />
+
+                <Text style={styles.label}>Seu idade atual:</Text>
+                <Input
+                    name='age'
+                    control={control}
+                    placeholder='Ex: 23'
+                    error={errors.age?.message}
+                    keyboardType='numeric'
+                />
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Avaçar</Text>
+                </TouchableOpacity>
 
             </ScrollView>
 
@@ -27,5 +89,23 @@ const styles = StyleSheet.create({
     content: {
         paddingLeft: 16,
         paddingRight: 16,
+    },
+    label: {
+        fontSize: 16,
+        color: colors.white,
+        fontWeight: 'bold',
+        marginBottom: 8
+    },
+    button: {
+        backgroundColor: colors.blue,
+        height: 44,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4
+    },
+    buttonText: {
+        color: colors.white,
+        fontSize: 16,
+        fontWeight: 'bold'
     }
 });
